9279c340125a80ac987d179b346f91efdf8457b2
engrogerio <engrogerio@users.noreply.github.com>
2022-01-09 20:27:28 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="engrogerio"
+original_name="sanit2"
+original_urlname="sanit2"
+original_description="Awesome sanit2 created by engrogerio"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv sanit2 $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- sanit2/app.py
+++ sanit2/app.py
@@ -23,18 +23,18 @@ def read(*paths, **kwargs):
 
 
 description = """
-project_name API helps you do awesome stuff. 🚀
+sanit2 API helps you do awesome stuff. 🚀
 """
 
 app = FastAPI(
-    title="project_name",
+    title="sanit2",
     description=description,
     version=read("VERSION"),
-    terms_of_service="http://project_name.com/terms/",
+    terms_of_service="http://sanit2.com/terms/",
     contact={
-        "name": "author_name",
-        "url": "http://project_name.com/contact/",
-        "email": "author_name@project_name.com",
+        "name": "engrogerio",
+        "url": "http://sanit2.com/contact/",
+        "email": "engrogerio@sanit2.com",
     },
     license_info={
         "name": "The Unlicense",
